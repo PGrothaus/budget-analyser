b4b858233619e4c7cfef4d451659dce4c7ddc742
PGrothaus <pgrothaus@github.com>
2020-04-21 20:57:25 -0400

    fix tests

--- tests/lib/test_parsers.py
+++ tests/lib/test_parsers.py
@@ -11,11 +11,11 @@ class ATransactionParser(unittest.TestCase):
         self.data = helper.load_transaction_data()
 
     def test_can_return_all_transactions(self):
-        self.parsed_data = parsers.parse_transactions(self.data)
+        self.parsed_data = parsers.parse_transactions_data(self.data)
         assert len(self.parsed_data) == 35
 
     def test_can_return_a_list_of_transactions(self):
-        self.parsed_data = parsers.parse_transactions(self.data)
+        self.parsed_data = parsers.parse_transactions_data(self.data)
         assert isinstance(self.parsed_data, list)
         for elem in self.parsed_data:
             assert isinstance(elem, Transaction)
