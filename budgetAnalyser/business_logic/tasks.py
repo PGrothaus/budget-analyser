faafca1732b023ac039ad236c4fa7eb1e744c600
PGrothaus <pgrothaus@github.com>
2020-07-17 00:28:06 -0400

    task to collect fintual data

--- budgetAnalyser/business_logic/tasks.py
+++ budgetAnalyser/business_logic/tasks.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from datetime import timezone
 
 from backend import models
 from background_task import background
 from business_logic import metrics
 from collector import collectors
 from custom_auth.models import MyUser
+from fintual import caller as fintual
 
 
 @background(schedule=1)
@@ -16,7 +18,7 @@ def calculate_networth_all_users():
         nw = metrics.networth(user)
         models.NetWorth.objects.create(user=user,
                                        value=nw,
-                                       valued_at=datetime.now(),
+                                       valued_at=datetime.now(timezone.utc),
                                        type="networth")
         print("NetWorth for user %s is %s" % (user_id, nw))
 
@@ -30,7 +32,7 @@ def calculate_total_retirement_all_users():
         nw = metrics.retirement(user)
         models.NetWorth.objects.create(user=user,
                                        value=nw,
-                                       valued_at=datetime.now(),
+                                       valued_at=datetime.now(timezone.utc),
                                        type="retirement")
         print("Total Retirement for user %s is %s" % (user_id, nw))
 
@@ -44,7 +46,7 @@ def calculate_total_retirement_investments_all_users():
         nw = metrics.retirement_investments(user)
         models.NetWorth.objects.create(user=user,
                                        value=nw,
-                                       valued_at=datetime.now(),
+                                       valued_at=datetime.now(timezone.utc),
                                        type="retirement_investment")
         print("Total Retirement Investment for user %s is %s" % (user_id, nw))
 
@@ -58,7 +60,7 @@ def calculate_savings_all_users():
         nw = metrics.savings(user)
         models.NetWorth.objects.create(user=user,
                                        value=nw,
-                                       valued_at=datetime.now(),
+                                       valued_at=datetime.now(timezone.utc),
                                        type="savings")
         print("All Savings for user %s is %s" % (user_id, nw))
 
@@ -72,7 +74,7 @@ def calculate_savings_investments_all_users():
         nw = metrics.savings_investments(user)
         models.NetWorth.objects.create(user=user,
                                        value=nw,
-                                       valued_at=datetime.now(),
+                                       valued_at=datetime.now(timezone.utc),
                                        type="savings_investment")
         print("All Savings Investment for user %s is %s" % (user_id, nw))
 
@@ -81,3 +83,25 @@ def calculate_savings_investments_all_users():
 def collect_all_exchange_rates():
     print("Collecting rates")
     collectors.collect_all()
+
+
+@background(schedule=1)
+def collect_fintual_data():
+    print("Collect Fintual Data")
+    user_ids = [1]
+    for user_id in user_ids:
+        user = MyUser.objects.get(pk=user_id)
+        goals = fintual.get_user_goals(user)
+        for goal in goals:
+            acc = models.Account.objects.get(
+                user=user,
+                name=goal["name"],
+            )
+            value = goal["value"]
+            valued_at = datetime.now(timezone.utc)
+            models.AccountValue.objects.create(
+                user=user,
+                account=acc,
+                value=value,
+                valued_at=valued_at,
+            )
