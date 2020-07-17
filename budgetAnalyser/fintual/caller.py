99a8d5f8b7af66c88ff1cbc97f337d495a68efad
PGrothaus <pgrothaus@github.com>
2020-07-16 23:13:52 -0400

    wrapper fct

--- budgetAnalyser/fintual/caller.py
+++ budgetAnalyser/fintual/caller.py
@@ -4,8 +4,21 @@ import requests
 import secrets
 
 
+def get_goals_all_users():
+    goals = []
+    for username in secrets.FINTUAL_USERS:
+        goals.extend(get_user_goals(username))
+    for goal in goals:
+        print(goal)
 
-def get_user_info(username):
+
+def get_user_goals(username):
+    token = get_token(username)
+    goals = get_goals(username, token)
+    return format_goals(username, goals)
+
+
+def get_token(username):
     email = secrets.FINTUAL_USERNAMES.get(username)
     pw = secrets.FINTUAL_PWS.get(username)
 
@@ -22,7 +35,11 @@ def get_user_info(username):
     )
 
     res = json.loads(response.text)
-    token = res["data"]["attributes"]["token"]
+    return res["data"]["attributes"]["token"]
+
+
+def get_goals(username, token):
+    email = secrets.FINTUAL_USERNAMES.get(username)
 
     headers = {
         'Accept': 'application/json',
@@ -34,6 +51,18 @@ def get_user_info(username):
 
     response = requests.get('https://fintual.cl/api/goals', headers=headers, params=params)
     goals = json.loads(response.text)["data"]
+    return [{
+        "deposited": datum["attributes"]["deposited"],
+        "value": datum["attributes"]["nav"]
+    } for datum in goals]
+
+
+def format_goals(username, goals):
     for goal in goals:
-        print(goal["attributes"]["deposited"])
-        print(goal["attributes"]["nav"])
+        goal["gain"] = goal["value"] - goal["deposited"]
+        goal["user"] = username
+    return goals
+
+
+if "__main__" == __name__:
+    get_goals_all_users()
